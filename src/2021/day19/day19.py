75c6dc75aa90244e821c027bcc037a12717d04c7
蔡斌卿 <caibinqing@live.com>
2022-01-20 17:19:42 +0800

    19-2

--- src/2021/day19/day19.py
+++ src/2021/day19/day19.py
@@ -51,7 +51,34 @@ def each_direction(v):
 
 all_directions = list(each_direction(np.identity(4, dtype=int)))
 
-def is_overlap(d1: BeaconsDistances, d2: BeaconsDistances, s1: Scanner, s2: Scanner):
+
+def load_data():
+    with open(path.join(path.dirname(__file__), "input.txt")) as f:
+        scanners: list[Scanner] = []
+
+        currentScannerId = -1
+        for line in f:
+            result = re.match(r"--- scanner (\d+) ---", line)
+            if result is not None:
+                currentScannerId = int(result.groups()[0])
+                scanners.append([])
+            elif line != "\n":
+                (x, y, z) = line.strip().split(',')
+                scanners[currentScannerId].append(np.array([int(x), int(y), int(z), 1], dtype=int))
+
+    for i in range(len(scanners)):
+        scanners[i] = np.array(scanners[i])
+
+    distances = [[[np.linalg.norm(beacon - b) for b in scanner] for beacon in scanner] for scanner in scanners]
+
+    return scanners, distances
+
+
+def get_connect_matrix(d1: BeaconsDistances, d2: BeaconsDistances, s1: Scanner, s2: Scanner):
+    '''
+    返回 s2 到 s1 的连接矩阵
+    s2.dot(mat) == s1
+    '''
     for i1, ds1 in enumerate(d1):
         for i2, ds2 in enumerate(d2):
             if len(np.intersect1d(ds1, ds2)) >= 12:
@@ -75,80 +102,79 @@ def is_overlap(d1: BeaconsDistances, d2: BeaconsDistances, s1: Scanner, s2: Scan
     return None
 
 
-def main():
-    with open(path.join(path.dirname(__file__), "input.txt")) as f:
-        scanners: list[Scanner] = []
-
-        currentScannerId = -1
-        for line in f:
-            result = re.match(r"--- scanner (\d+) ---", line)
-            if result is not None:
-                currentScannerId = int(result.groups()[0])
-                scanners.append([])
-            elif line != "\n":
-                (x, y, z) = line.strip().split(',')
-                scanners[currentScannerId].append(np.array([int(x), int(y), int(z), 1], dtype=int))
-
-    for i in range(len(scanners)):
-        scanners[i] = np.array(scanners[i])
-
-    distances = [[[np.linalg.norm(beacon - b) for b in scanner] for beacon in scanner] for scanner in scanners]
-
+def resolve_xf(scanners, distances):
+    '''
+    返回sn到s0的连接矩阵
+    '''
     xf = dict()
     xf[0] = np.identity(4, dtype=int)
     pending = []
+
+    def resolve_pair(i, j):
+        if i == j: return
+        if i in xf and j in xf:
+            return
+        if i in xf:
+            mat = get_connect_matrix(distances[i], distances[j], scanners[i], scanners[j])
+            if mat is not None:
+                # print(f"{j}-{i}: {mat}")
+                xf[j] = mat.dot(xf[i])
+                print(f"{j}: {xf[j]}")
+        elif j in xf:
+            mat = get_connect_matrix(distances[j], distances[i], scanners[j], scanners[i])
+            if mat is not None:
+                # print(f"{i}-{j}: {mat}")
+                xf[i] = mat.dot(xf[j])
+                print(f"{i}: {xf[i]}")
+        else:
+            pending.append((i, j))
+
     count = len(distances)
     for i in range(count):
         for j in range(count):
-            if i == j: continue
-            if i in xf and j in xf:
-                continue
-            if i in xf:
-                mat = is_overlap(distances[i], distances[j], scanners[i], scanners[j])
-                if mat is not None:
-                    # print(f"{j}-{i}: {mat}")
-                    xf[j] = mat.dot(xf[i])
-                    print(f"{j}: {xf[j]}")
-            elif j in xf:
-                mat = is_overlap(distances[j], distances[i], scanners[j], scanners[i])
-                if mat is not None:
-                    # print(f"{i}-{j}: {mat}")
-                    xf[i] = mat.dot(xf[j])
-                    print(f"{i}: {xf[i]}")
-            else:
-                pending.append((i, j))
+            resolve_pair(i, j)
 
     while len(pending) > 0:
-        pending_ = pending.copy()
+        pending_copy = pending.copy()
         pending = []
-        for i, j in pending_:
-            if i in xf and j in xf:
-                continue
-            if i in xf:
-                mat = is_overlap(distances[i], distances[j], scanners[i], scanners[j])
-                if mat is not None:
-                    # print(f"{j}-{i}: {mat}")
-                    xf[j] = mat.dot(xf[i])
-                    print(f"{j}: {xf[j]}")
-            elif j in xf:
-                mat = is_overlap(distances[j], distances[i], scanners[j], scanners[i])
-                if mat is not None:
-                    # print(f"{i}-{j}: {mat}")
-                    xf[i] = mat.dot(xf[j])
-                    print(f"{i}: {xf[i]}")
-            else:
-                pending.append((i, j))
+        for i, j in pending_copy:
+            resolve_pair(i, j)
 
+    return xf
+
+
+def manhhattan(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]) + abs(p1[2] - p2[2])
+
+
+def main():
+    scanners, distances = load_data()
+
+    xf = resolve_xf(scanners, distances)
     # print(xf)
+
     all_beacons = []
     for idx, scanner in enumerate(scanners):
         for beacon in scanner:
             p = list(beacon.dot(xf[idx]))
             if p not in all_beacons:
                 all_beacons.append(p)
-            else:
-                pass
     print(f"Q1: {len(all_beacons)}")
+
+    positions = []
+    total = len(scanners)
+    for i in range(total):
+        position = (-np.array([0,0,0,1], dtype=int).dot(xf[i]))[:3]
+        print(f"{i}: {position}")
+        positions.append(position)
+
+    max = 0
+    for i in range(total):
+        for j in range(i + 1, total):
+            dis = manhhattan(positions[i], positions[j])
+            if dis > max:
+                max = dis
+    print(f"Q2: {max}")
 
 
 if __name__ == "__main__":
